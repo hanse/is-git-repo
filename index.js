c7defae04fd3377638a640d4f47e006b419bbdb9
Hanse <hanse19@gmail.com>
2014-05-10 21:25:52 +0200

    use async.some

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var async = require('async');
 
 module.exports = function(file, fn) {
-  async.filter([file + '/.git', file + '/HEAD'], fs.exists, function(exists) {
-    fn(exists.length > 0);
+  async.some([file + '/.git', file + '/HEAD'], fs.exists, function(exists) {
+    fn(exists);
   });
 };
